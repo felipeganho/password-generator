ba6593532bc951fb90eadfa5292c89e6001997b9
Felipe Silva Ganho <felipeganho@gmail.com>
2024-04-20 10:05:08 -0300

    feat: add home to generate password and start modal

---
+++ src/components/modal/index.js
@@ -0,0 +1,15 @@
+import { View, StyleSheet, Text } from "react-native";
+
+export function ModalPassword(){
+    return (
+        <View>
+            <Text>Teste modal</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
